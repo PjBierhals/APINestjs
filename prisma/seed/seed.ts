49c6a03889a75a5a46554fb16ff83e17277472b7
PjBierhals <pjbierhals@gmail.com>
2025-05-01 14:26:57 -0300

    primeiro comite criação da configurações e banco da API

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,68 @@
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { BcryptService } from 'src/modules/auth/hashing/bcrypt.service';
+
+const prisma = new PrismaService();
+const bcryptService = new BcryptService(); // Inicializando o serviço de hashing
+enum PERMISSION {
+  MASTER = 'MASTER',
+}
+async function main() {
+  console.log('Iniciando o seed...');
+  // Criando a seção MasterTI
+  const masterSection = await prisma.section.create({
+    data: {
+      authorId: '', // Será atualizado após a criação do usuário
+      description: 'Sessão Master',
+    },
+  });
+
+  // Criando a posição padrão
+  const masterPosition = await prisma.position.create({
+    data: {
+      authorId: '', // Será atualizado após a criação do usuário
+      description: 'Administrador Master',
+    },
+  });
+
+  //hash no password
+  const passwordH = await bcryptService.hash('masterpassword');
+  // Criando o usuário MasterTI (autor)
+  const masterUser = await prisma.user.create({
+    data: {
+      email: 'master@sisgget.com',
+      password: passwordH, // Utilize hash para senhas em produção
+      name: 'Master TI',
+      status: true,
+      permission: PERMISSION.MASTER,
+      positionId: masterPosition.id,
+      sectionId: masterSection.id,
+    },
+  });
+
+  // Atualizando a seção e a posição com o ID do autor
+  await prisma.section.update({
+    where: { id: masterSection.id },
+    data: {
+      authorId: masterUser.id,
+    },
+  });
+
+  await prisma.position.update({
+    where: { id: masterPosition.id },
+    data: {
+      authorId: masterUser.id,
+    },
+  });
+
+  console.log('Seed concluído com sucesso!');
+  console.log({ masterUser, masterPosition, masterSection });
+}
+
+main()
+  .catch((e) => {
+    console.error('Erro ao executar o seed:', e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { PositionsModule } from './modules/positions/positions.module';
+import { SectorsModule } from './modules/sectors/sectors.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './database/prisma/prisma.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PositionsModule,
+    PrismaModule,
+    SectorsModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
